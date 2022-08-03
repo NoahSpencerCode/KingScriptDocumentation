bcc992dee6fcef9e57ba3c82311157f28dc8a418
NoahSpencerCode <noah@BigMac.askey.com>
2022-08-03 09:20:03 -0700

    Textfield

--- kingscriptfrontend/src/App.js
+++ kingscriptfrontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import ResponsiveAppBar from './components/ResponsiveAppBar';
+import Addition from './components/Addition'
 
 function App() {
   return (
     <div className="App">
       <ResponsiveAppBar />
+      <Addition />
     </div>
   );
 }
