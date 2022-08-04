dc8fb4a5700dbf90ac5ca85d3a44e046408e9f13
NoahSpencerCode <noah@BigMac.askey.com>
2022-08-04 15:00:18 -0700

    styled page

--- kingscriptfrontend/src/components/Addition.js
+++ kingscriptfrontend/src/components/Addition.js
@@ -4,7 +4,7 @@ import { Container ,Paper,Button} from '@mui/material';
 
 
 export default function Addition() {
-    const paperStyle={padding:'50px 20px', width:600,margin:"20px auto"}
+    const paperStyle={padding:'50px 20px', width:600,margin:"20px auto",backgroundColor: "rgb(30,38,45)"}
     const[title,setTitle]=useState('')
     const[body,setBody]=useState('')
     const[additions,setAdditions]=useState([])
@@ -35,7 +35,7 @@ useEffect(()=>{
 
     <Container>
         <Paper elevation={3} style={paperStyle}>
-            <h1 style={{color:"blue"}}><u>Add Addition</u></h1>
+            <h1>New Addition</h1>
 
     <form noValidate autoComplete="off">
     
@@ -53,22 +53,16 @@ useEffect(()=>{
     </form>
    
     </Paper>
-    <h1>Additions</h1>
+    <h1>Documentation</h1>
 
-    <Paper elevation={3} style={paperStyle}>
-
-      {additions.map(addition=>(
-        <Paper elevation={6} style={{margin:"10px",padding:"15px", textAlign:"left"}} key={addition.id}>
-         Id:{addition.id}<br/>
-         Title:{addition.title}<br/>
-         Body:{addition.body}
+    {additions.map(addition=>(
+        <Paper elevation={6} style={{margin:"10px",padding:"15px", textAlign:"left",backgroundColor: "rgb(30,38,45)"}} key={addition.id}>
+         
+         <h2>{addition.title}</h2>
+         <p>{addition.body}</p>
 
         </Paper>
-      ))
-}
-
-
-    </Paper>
+      ))}
 
 
 
