15aba4a37cac17214d912dde01c8d66bb8e7bf38
NoahSpencerCode <noah@BigMac.askey.com>
2022-08-05 14:24:22 -0700

    about route

---
+++ kingscriptfrontend/src/components/About.js
@@ -0,0 +1,16 @@
+
+
+export default function About() {
+
+
+    return (
+        <div>
+            <h1 style={{fontSize:'5em', width:'.3fr'}}>Welcome to <br/> KingScript</h1>
+            <div class="multiline colored">
+                KingScript is a Roblox Studio package manager and command line interface. <br/><br/>
+                Using KingScript helps improve development times dramatically!
+            </div>
+        </div>
+    )
+}
+
