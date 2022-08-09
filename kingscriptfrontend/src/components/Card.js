6c23bd178b5c5f0a3b017b5c2e752b65a562e4c4
NoahSpencerCode <noah@BigMac.askey.com>
2022-08-09 09:10:11 -0700

    Edit and delete buttons

--- kingscriptfrontend/src/components/Card.js
+++ kingscriptfrontend/src/components/Card.js
@@ -5,14 +5,22 @@ import { Paper, Button } from '@mui/material';
 export default function Card (props) {
 
     return (
-        <Paper elevation={6} style={{margin:"10px",padding:"15px", textAlign:"left",backgroundColor: "rgb(30,38,45)"}} key={props.id}>
+        <Paper elevation={6} style={{margin:"10px",padding:"15px", textAlign:"left",backgroundColor: "rgb(30,38,45)", display:'flex', justifyContent: 'space-between'}} key={props.id}>
         
+        <div>
+
         <h2>{props.title}</h2>
         <div class="multiline colored">
             {props.body}
         </div>
 
-        <Button color='secondary' variant="outlined" href='/Documentation'>Documentation {'>'}</Button>
+        </div>
+
+        <div>
+
+        <Button sx={{height:'40px', marginRight:'10px'}} color='secondary' variant="contained" href='/Documentation'>Edit</Button>
+        <Button sx={{height:'40px', backgroundColor:'red'}} color='secondary' variant="contained" href='/Documentation'>Delete</Button>
+        </div>
 
         </Paper>
     )
