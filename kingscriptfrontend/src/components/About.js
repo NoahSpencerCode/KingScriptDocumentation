d892f3ed914feaace335c5b8ea63136b20bbc41f
NoahSpencerCode <noah@BigMac.askey.com>
2022-08-11 10:04:38 -0700

    indentations

--- kingscriptfrontend/src/components/About.js
+++ kingscriptfrontend/src/components/About.js
@@ -5,7 +5,6 @@ import {Dvr, AddCard, Terminal} from '@mui/icons-material'
 
 export default function About() {
 
-
     return (
         <>
             <div>
@@ -16,7 +15,9 @@ export default function About() {
                 </div>
             </div>
             <div style={{backgroundColor:'rgb(30,38,45)', boxShadow:'0px 0px 20px',padding:'30px 30px'}}>
+
                 <h2 style={{fontSize:'2em', padding:'0px 0px'}}>Installing KingScript to your Studio</h2>
+
                 <p>You can purchase the KingScript plugin at the Creator Marketplace here!</p>
                 
                 <Button color='secondary' variant="outlined" href=''>Get Plugin {'>'}</Button>
@@ -27,16 +28,19 @@ export default function About() {
 
             </div>
             <div style={{padding:'40px 40px'}}>
+                
                 <h1 style={{fontSize:'3em', width:'.3fr'}}>Features</h1>
                 
                 <IconButton href='/Documentation' sx={{flexDirection:'column', margin: '30px 30px'}}>
                     <AddCard sx={{color: 'purple', fontSize: '3em'}}/>
                     <p>Modules</p>
                 </IconButton>
+                
                 <IconButton href='/Documentation' sx={{flexDirection:'column', margin: '30px 30px'}}>
                     <Terminal sx={{color: 'purple', fontSize: '3em'}}/>
                     <p>Command Line</p>
                 </IconButton>
+
                 <IconButton href='/Documentation' sx={{flexDirection:'column', margin: '30px 30px'}}>
                     <Dvr sx={{color: 'purple', fontSize: '3em'}}/>
                     <p>Systems</p>
--- kingscriptfrontend/src/components/Addition.js
+++ kingscriptfrontend/src/components/Addition.js
@@ -2,40 +2,42 @@ import React, { useEffect, useState } from 'react';
 import TextField from '@mui/material/TextField';
 import { Container ,Paper,Button} from '@mui/material';
 import Card from './Card'
-//import { styled } from '@mui/material/styles';
-
 
 
 export default function Addition() {
-    const paperStyle={padding:'50px 20px',margin:"20px auto",backgroundColor: "rgb(30,38,45)"}
-    const[title,setTitle]=useState('')
-    const[body,setBody]=useState('')
-    const[additions,setAdditions]=useState([])
+  const paperStyle={padding:'50px 20px',margin:"20px auto",backgroundColor: "rgb(30,38,45)"}
+  const[title,setTitle]=useState('')
+  const[body,setBody]=useState('')
+  const[additions,setAdditions]=useState([])
 
   const handleClick=(e)=>{
+
     e.preventDefault()
     const addition={title,body}
     console.log(addition)
+
     fetch("http://localhost:8080/addition/add",{
+
       method:"POST",
       headers:{"Content-Type":"application/json"},
       body:JSON.stringify(addition)
 
-  }).then(()=>{
-    console.log("New Addition added")
-    window.location.reload(false);
+    }).then(()=>{
 
-  })
-}
+      console.log("New Addition added")
+      window.location.reload(false);
 
-useEffect(()=>{
-  fetch("http://localhost:8080/addition/getAll")
-  .then(res=>res.json())
-  .then((result)=>{
-    setAdditions(result);
-  }
-)
-},[])
+    })
+  }   
+
+  useEffect(()=>{
+    fetch("http://localhost:8080/addition/getAll")
+    .then(res=>res.json())
+    .then((result)=>{
+      setAdditions(result);
+    }
+  )
+  },[])
 
 
   return (
@@ -44,31 +46,25 @@ useEffect(()=>{
         <Paper elevation={3} style={paperStyle}>
             <h1>New Addition</h1>
 
-    <form noValidate autoComplete="off">
+          <form noValidate autoComplete="off">
     
-      <TextField id="outlined-basic" label="Title" variant="outlined" fullWidth style={{margin: '10px 0'}}
-      value={title}
-      onChange={(e)=>setTitle(e.target.value)}
-      
-      />
-      <TextField id="outlined-basic" label="Body" variant="outlined" fullWidth style={{margin: '10px 0'}} multiline rows={10}
-      value={body}
-      onChange={(e)=>setBody(e.target.value)}
-      />
-      <Button variant="contained" color="secondary" onClick={handleClick}>
-  Submit
-</Button>
-    </form>
+            <TextField id="outlined-basic" label="Title" variant="outlined" fullWidth style={{margin: '10px 0'}}
+              value={title}
+              onChange={(e)=>setTitle(e.target.value)}
+            />
+            <TextField id="outlined-basic" label="Body" variant="outlined" fullWidth style={{margin: '10px 0'}} multiline rows={10}
+              value={body}
+              onChange={(e)=>setBody(e.target.value)}
+            />
+            <Button variant="contained" color="secondary" onClick={handleClick}>Submit</Button>
+          </form>
    
-    </Paper>
-    <h1>Documentation</h1>
+        </Paper>
+        <h1>Documentation</h1>
 
-    {additions.map(addition=>(
-        <Card id={addition.id} title={addition.title} body={addition.body}/>
-      ))}
-
-
-
+        {additions.map(addition=>(
+          <Card id={addition.id} title={addition.title} body={addition.body}/>
+        ))}
     </Container>
   );
 }
